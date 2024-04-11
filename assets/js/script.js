5ac01173a94ca67befba9a0c3e74c07253ef592e
Mitra USYD <mitraahmadi129@gmail.com>
2024-04-11 20:05:35 +1000

    pseudo functions in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,33 @@
-function renderPopup()
+let popupEl = document.querySelector('#INSERT ID HERE');
+
+// CREATE NON-NATIVE POPUP FOR WHEN FILTERS ARE EMPTY
+function renderPopup(event)
+event.preventDefault();
+console.log(event);
+
+if (usernameInput.value === "") {
+  alert("Please give yourself a username!"); // REPLACE WITH NON-NATIVE POPUP NOT ALERT (SHOW/HIDE METHODS)
+};
+popupEl.addEventListener("click", renderPopup);
+
+
+// FUNCTION TO RETRIEVE USER DATA FROM LOCAL STORAGE
+function storeUser() {
+    // add loop for array to make sure each blog is added and not replaced
+  
+    let newUser = {
+      userName: usernameInput.value,
+    };
+  
+    let userData = JSON.parse(localStorage.getItem("NAME OF LOCAL")) ?? [];
+  
+    userData.push(newUser);
+  
+    console.log(userData);
+    let userDataStringedUp = JSON.stringify(userData);
+    localStorage.setItem("NAME", userDataStringedUp);
+};
+
+  // CREATE SUBMIT FILTER OPTION FUNCTION
+  // TEXTBOX FOR FOOD NAME
+  // DROPDOWN FOR FILTER
