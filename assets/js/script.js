8af108a20c6c242f5f3e58b3672bb34fee77e1d8
Mitra USYD <mitraahmadi129@gmail.com>
2024-04-18 19:27:09 +1000

    adding new css and js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,212 @@
-let popupEl = document.querySelector('#INSERT ID HERE');
+const ytAPIKey = "AIzaSyCawopGL82AFkgjtzzGG56lw1ZIb4HZcmQ";
+const spoonAPIKey = `7a974e772bec455da7a065c595ebe2b3`;
 
-// CREATE NON-NATIVE POPUP FOR WHEN FILTERS ARE EMPTY
-function renderPopup(event)
-event.preventDefault();
-console.log(event);
+const modalEl = document.getElementById(`search-modal`);
+const modalButton = document.getElementById(`modal-button`);
+const modalClose = document.getElementById(`close-modal`);
+const formClose = document.getElementById(`close-form`);
+const inputEl = document.getElementById(`input-ingredient`);
+const formEl = document.getElementById(`search-form`);
+const recipeNameEl = document.getElementById(`recipe-name`);
+const recipeInfoEl = document.getElementById(`recipe-info`);
+const recipeIngEl = document.getElementById(`recipe-ingredients`);
+const recipePicEl = document.getElementById(`recipe-picture`);
+const createdRecipesEl = document.getElementById(`recipe-buttons`);
+const createdButtonsEl = document.getElementsByClassName(`created-buttons`);
+const recipeSumBoxEl = document.getElementById(`recipe-info-container`);
 
-if (usernameInput.value === "") {
-  alert("Please give yourself a username!"); // REPLACE WITH NON-NATIVE POPUP NOT ALERT (SHOW/HIDE METHODS)
-};
-popupEl.addEventListener("click", renderPopup);
+let recipeNames = JSON.parse(localStorage.getItem(`recipes`));
+if (recipeNames === null) {
+  recipeNames = [];
+}
 
+function saveRecipes(recipes) {
+  localStorage.setItem(`recipes`, JSON.stringify(recipes));
+}
 
-// FUNCTION TO RETRIEVE USER DATA FROM LOCAL STORAGE
-function storeUser() {
-    // add loop for array to make sure each blog is added and not replaced
-  
-    let newUser = {
-      userName: usernameInput.value,
-    };
-  
-    let userData = JSON.parse(localStorage.getItem("NAME OF LOCAL")) ?? [];
-  
-    userData.push(newUser);
-  
-    console.log(userData);
-    let userDataStringedUp = JSON.stringify(userData);
-    localStorage.setItem("NAME", userDataStringedUp);
-};
+function openModal() {
+  modalEl.classList.add(`is-active`);
+}
 
-  // CREATE SUBMIT FILTER OPTION FUNCTION
-  // TEXTBOX FOR FOOD NAME
-  // DROPDOWN FOR FILTER
+function closeModal() {
+  modalEl.classList.remove(`is-active`);
+}
+
+modalButton.addEventListener(`click`, function (event) {
+  // console.log(`it clicked`);
+  openModal();
+});
+
+modalClose.addEventListener(`click`, function (event) {
+  closeModal();
+});
+
+formClose.addEventListener(`click`, function (event) {
+  closeModal();
+});
+
+function createButtons(data) {
+  if (newSearch === 1) {
+    for (let i = 0; i < data.length; i++) {
+      let name = data[i].title;
+      let recipeId = data[i].id;
+      let listEl = document.createElement(`li`);
+      let buttonEl = document.createElement(`button`);
+      // buttonEl.setAttribute(`type`, `button`);
+      buttonEl.textContent = name;
+      buttonEl.setAttribute(`id`, recipeId);
+      buttonEl.classList = `created-buttons`;
+      listEl.appendChild(buttonEl);
+      createdRecipesEl.appendChild(listEl);
+    }
+  }
+
+  //make buttons display info when clicked
+  buttonInit();
+}
+
+//work on this
+function clearDisplay() {}
+
+function createRecipeInfo(data) {
+  let rName = data.title;
+  let rImage = data.image;
+  let instructions = data.instructions;
+  let summary = data.summary;
+
+  // console.log(summary);
+
+  for (let i = 0; i < data.extendedIngredients.length; i++) {
+    let instEl = document.createElement(`p`);
+    instEl.textContent = data.extendedIngredients[i].original;
+    recipeIngEl.appendChild(instEl);
+  }
+
+  recipeNameEl.textContent = rName;
+  recipePicEl.setAttribute(`src`, rImage);
+
+  let recipeInfo = document.createElement("p");
+  recipeInfo.innerHTML = summary;
+  recipeSumBoxEl.appendChild(recipeInfo);
+
+  let infoEl = document.createElement(`p`);
+  infoEl.innerHTML = instructions;
+  recipeInfoEl.appendChild(infoEl);
+}
+
+//api caller when form is submitted
+function spoonAPICallerInit(url) {
+  fetch(url)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          // console.log(data);
+          //id:videoID gives youtube.com/watch?v=[videoID]
+          // createRecipeInfo(data);
+          createButtons(data);
+        });
+      } else {
+        alert(`Error: ${response.statusText}`);
+      }
+    })
+    .catch(function (error) {
+      alert(`Unable to connect to API`);
+    });
+}
+
+//api caller when button is clicked
+function spoonAPICallerButton(url) {
+  fetch(url)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          console.log(data);
+          //id:videoID gives youtube.com/watch?v=[videoID]
+          createRecipeInfo(data);
+        });
+      } else {
+        alert(`Error: ${response.statusText}`);
+      }
+    })
+    .catch(function (error) {
+      alert(`Unable to connect to API`);
+    });
+}
+
+formEl.addEventListener(`submit`, function (event) {
+  event.preventDefault();
+
+  // console.log(inputEl.value);
+
+  if (!inputEl.value) {
+    console.log(`nothign entered`);
+    closeModal();
+    inputEl.value = ``;
+  } else {
+    // console.log(inputEl.value);
+    let spoonInput = inputEl.value;
+
+    newSearch = 1;
+
+    let apiURL = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${spoonInput}&number=5&apiKey=${spoonAPIKey}`;
+
+    closeModal();
+    inputEl.value = ``;
+
+    spoonAPICallerInit(apiURL);
+  }
+});
+
+function buttonInit() {
+  for (let i = 0; i < createdButtonsEl.length; i++) {
+    createdButtonsEl[i].addEventListener(`click`, function (event) {
+      // console.log(`it works`);
+
+      let recipeId = createdButtonsEl[i].id;
+
+      let buttonIDAPI = `https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=${spoonAPIKey}`;
+
+      spoonAPICallerButton(buttonIDAPI);
+
+      // console.log(recipeId);
+    });
+  }
+  // console.log(createdButtonsEl.length);
+}
+
+// CREATE SUBMIT FILTER OPTION FUNCTION
+// TEXTBOX FOR FOOD NAME
+// DROPDOWN FOR FILTER
+
+bulmaCarousel.attach("#slider", {
+  slidesToScroll: 1,
+  slidesToShow: 1,
+  infinite: true,
+  autoplay: true,
+});
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("item-slide");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+}
